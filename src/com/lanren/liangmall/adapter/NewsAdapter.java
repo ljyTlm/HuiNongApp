0214715e57313a9358641fa9a2505cbd2493663d
ljyTlm <970955947@qq.com>
2018-11-23 00:34:42 +0800

    这是对上一次提交的补充 并未添加新内容

---
+++ src/com/lanren/liangmall/adapter/NewsAdapter.java
@@ -0,0 +1,71 @@
+package com.lanren.liangmall.adapter;
+
+import java.util.List;
+
+import com.lanren.liangmall.R;
+import com.lanren.liangmall.entity.NewsEntity;
+import com.lanren.liangmall.net.NetHttpData;
+import com.loopj.android.image.SmartImageView;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class NewsAdapter extends BaseAdapter {
+
+	 private List<NewsEntity> newsList;//数据源
+	 private LayoutInflater mInflater;//布局装载器对象
+	 
+	 // 通过构造方法将数据源与数据适配器关联起来
+	 // context:要使用当前的Adapter的界面对象
+	 public NewsAdapter (Context context, List<NewsEntity> list) {
+        newsList = list;
+        mInflater = LayoutInflater.from(context);
+    }
+	
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return newsList.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return newsList.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View arg1, ViewGroup arg2) {
+		// TODO Auto-generated method stub
+		//将布局文件转化为View对象
+        View view = mInflater.inflate(R.layout.news_home_itme,null);
+
+        /**
+         * 找到item布局文件中对应的控件 
+         */
+        SmartImageView imageView = (SmartImageView) view.findViewById(R.id.news_imge);
+        TextView titleTextView = (TextView) view.findViewById(R.id.news_title);
+
+        //获取相应索引的ItemBean对象
+        NewsEntity news = newsList.get(position);
+
+        /**
+         * 设置控件的对应属性值 
+         */
+        imageView.setImageUrl(NetHttpData.dataIp+ "/HuiNong/images/" + news.getImgName());
+        titleTextView.setText(news.getTitle());
+
+        return view;
+	}
+
+}
---
+++ src/com/lanren/liangmall/entity/NewsEntity.java
@@ -0,0 +1,29 @@
+package com.lanren.liangmall.entity;
+
+import android.graphics.drawable.Drawable;
+
+public class NewsEntity {
+	private String title;
+	private String imgName;
+	private String newsUrl;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getImgName() {
+		return imgName;
+	}
+	public void setImgName(String imgName) {
+		this.imgName = imgName;
+	}
+	public String getNewsUrl() {
+		return newsUrl;
+	}
+	public void setNewsUrl(String newsUrl) {
+		this.newsUrl = newsUrl;
+	}
+	
+}
